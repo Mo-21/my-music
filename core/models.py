635483fd0d1e8c604ba1254892c4c6b698287cdd
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-21 23:08:29 +0300

    make the first migration

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractBaseUser):
+class User(AbstractUser):
     email = models.EmailField(unique=True)
