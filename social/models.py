c9bb61522d88f2f78b939e39472e4e03489ab568
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-21 23:52:42 +0300

    create user and post models

--- social/models.py
+++ social/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Person(models.Model):
+    MEMBERSHIP_REGULAR = 'R'
+    MEMBERSHIP_PRIME = 'P'
+
+    MEMBERSHIP_CHOICES = [(MEMBERSHIP_REGULAR, 'Regular'),
+                          (MEMBERSHIP_PRIME, 'Prime')]
+
+    phone = models.CharField(max_length=255)
+    birthdate = models.DateField(null=True)
+    membership_status = models.CharField(
+        max_length=1, default=MEMBERSHIP_REGULAR, choices=MEMBERSHIP_CHOICES)
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(Person, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
