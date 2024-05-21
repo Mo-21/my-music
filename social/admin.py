364e43a33c23fdb752806a66aaf2ffef072874a8
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-22 01:51:20 +0300

    register customer at the admin site

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership_status']
+    list_editable = ['membership_status']
+    ordering = ['user__first_name', 'user__last_name']
+    list_select_related = ['user']
+    list_per_page = 10
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
--- social/models.py
+++ social/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib import admin
 
 
 class Customer(models.Model):
@@ -16,6 +17,14 @@ class Customer(models.Model):
 
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
 
 
 class Post(models.Model):
