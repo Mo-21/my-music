66e6be49c50bc4108e718a863e408ea67f5c8a04
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-29 01:28:13 +0300

    create music app models

--- server/music/models.py
+++ server/music/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    duration = models.SmallIntegerField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now=True)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class PlaylistItem(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.PROTECT)
+    song = models.ForeignKey(Song, on_delete=models.PROTECT)
