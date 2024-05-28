8fc9e0750f206c047fa8e99d4b7e3becdc40a542
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-29 02:27:15 +0300

    create customer profile at /me/

--- server/social/views.py
+++ server/social/views.py
@@ -1,13 +1,29 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import CustomerSerializer, PostSerializer
 from .models import Customer, Post
 
 
 class CustomerViewSet(ModelViewSet):
-    queryset = Customer.objects.prefetch_related('user').all()
+    queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = [IsAdminUser]
+
+    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        customer = Customer.objects.get(
+            user_id=request.user.id
+        )
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
 
 
 class PostViewSet(ModelViewSet):
