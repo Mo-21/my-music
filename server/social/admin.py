3ff232b90be981515d879b3217a11800f5b1a021
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-28 20:49:31 +0300

    unregister customer model

--- server/social/admin.py
+++ server/social/admin.py
@@ -1,12 +1,2 @@
 from django.contrib import admin
 from . import models
-
-
-@admin.register(models.Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership_status']
-    list_editable = ['membership_status']
-    ordering = ['user__first_name', 'user__last_name']
-    list_select_related = ['user']
-    list_per_page = 10
-    search_fields = ['first_name__istartswith', 'last_name__istartswith']
