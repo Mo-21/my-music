e308dceed0b1569d472d1af9c402c8b653881a8e
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-29 00:34:20 +0300

    allow access to admin only

--- server/social/views.py
+++ server/social/views.py
@@ -1,13 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
 from .serializers import CustomerSerializer, PostSerializer
 from .models import Customer, Post
-
-# TODO: List only accessible for admins
 
 
 class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.prefetch_related('user').all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAdminUser]
 
 
 class PostViewSet(ModelViewSet):
