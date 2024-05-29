0ac3c18c25850315ef4190124b237b414b1a3578
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-29 17:54:23 +0300

    create a LocalUserSerializer with get_user_model

--- server/social/serializers.py
+++ server/social/serializers.py
@@ -1,14 +1,28 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import Customer, Post
 
 
+User = get_user_model()
+
+
+class LocalUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email']
+
+
 class CustomerSerializer(serializers.ModelSerializer):
+    user = LocalUserSerializer()
+
     class Meta:
         model = Customer
-        fields = ['id', 'user_id', 'phone', 'birthdate', 'membership_status']
+        fields = ['id', 'user', 'phone', 'birthdate', 'membership_status']
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = CustomerSerializer()
+
     class Meta:
         model = Post
-        fields = ['id', 'content', 'user', 'created_at']
+        fields = ['id', 'content', 'author', 'created_at']
