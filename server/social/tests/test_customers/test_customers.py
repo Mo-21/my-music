1ca14b9a3684d73743af4d7d47bbaad601505dc0
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-29 05:44:25 +0300

    rename fixture

--- server/social/tests/test_customers/test_customers.py
+++ server/social/tests/test_customers/test_customers.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from django.forms.models import model_to_dict
+from django.conf import settings
 from model_bakery import baker
 from social.models import Customer
 import pytest
@@ -26,16 +27,16 @@ class TestCustomers:
 
         assert response.status_code == status.HTTP_200_OK
 
-    def test_create_customer_return_401_if_not_auth(self, api_client, create_customer):
-        customer = create_customer()
+    def test_create_customer_return_401_if_not_auth(self, api_client, create_customer_model):
+        customer = create_customer_model()
 
         response = api_client.post(
             '/social/customers/', model_to_dict(customer))
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_create_customer_return_201_if_auth(self, api_client, create_customer, authenticate_user):
-        customer = create_customer()
+    def test_create_customer_return_201_if_auth(self, api_client, create_customer_model, authenticate_user):
+        customer = create_customer_model()
 
         authenticate_user()
         response = api_client.post(
@@ -43,15 +44,15 @@ class TestCustomers:
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_get_customer_profile_return_401_if_not_auth(self, api_client, create_customer):
-        customer = create_customer()
+    def test_get_customer_profile_return_401_if_not_auth(self, api_client, create_customer_model):
+        customer = create_customer_model()
 
         response = api_client.get(f'/social/customers/{customer.id}/')
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_get_customer_profile_return_200_if_auth(self, api_client, create_customer, authenticate_user):
-        customer = create_customer()
+    def test_get_customer_profile_return_200_if_auth(self, api_client, create_customer_model, authenticate_user):
+        customer = create_customer_model()
 
         authenticate_user()
         response = api_client.get(f'/social/customers/{customer.id}/')
