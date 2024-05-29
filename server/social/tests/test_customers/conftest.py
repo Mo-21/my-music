0e5c187b0466064124ac3d6c3f72e4e069bf12cf
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-29 16:25:20 +0300

    remove unnecessary fixtures

--- server/social/tests/test_customers/conftest.py
+++ server/social/tests/test_customers/conftest.py
@@ -1,4 +1,3 @@
-from django.forms.models import model_to_dict
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 from model_bakery import baker
@@ -12,34 +11,12 @@ def api_client():
 
 
 @pytest.fixture
-def create_customer_model(api_client):
-    def do_create_customer_model():
-        customer = baker.make(Customer)
-        customer.birthdate = ''
-        return customer
-    return do_create_customer_model
-
-
-@pytest.fixture
-def create_customer(api_client, authenticate_user):
+def create_customer():
     def do_create_customer():
         customer = baker.make(Customer)
         customer.birthdate = ''
-
-        authenticate_user(is_staff=True)
-
-        api_client.post('/social/customers/', model_to_dict(customer))
-
         return customer
     return do_create_customer
-
-
-@pytest.fixture
-def create_user(api_client):
-    def do_create_user():
-        user = baker.make(User)
-        api_client.post('/auth/users/', model_to_dict(user))
-    return do_create_user
 
 
 @pytest.fixture
