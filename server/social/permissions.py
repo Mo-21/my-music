590f118f0e736eee3bfacb2adf676412c12a3431
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-29 16:25:01 +0300

    replace has_permission with has_object_permission

--- server/social/permissions.py
+++ server/social/permissions.py
@@ -9,7 +9,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user == obj.user
