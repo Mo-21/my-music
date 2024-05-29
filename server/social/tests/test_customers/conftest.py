5de1afcdec78cabaa6cafccaa7f1db54322f5955
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-29 05:44:15 +0300

    add create customer and user

--- server/social/tests/test_customers/conftest.py
+++ server/social/tests/test_customers/conftest.py
@@ -12,12 +12,34 @@ def api_client():
 
 
 @pytest.fixture
-def create_customer(api_client):
-    def do_create_customer():
+def create_customer_model(api_client):
+    def do_create_customer_model():
         customer = baker.make(Customer)
         customer.birthdate = ''
         return customer
+    return do_create_customer_model
+
+
+@pytest.fixture
+def create_customer(api_client, authenticate_user):
+    def do_create_customer():
+        customer = baker.make(Customer)
+        customer.birthdate = ''
+
+        authenticate_user(is_staff=True)
+
+        api_client.post('/social/customers/', model_to_dict(customer))
+
+        return customer
     return do_create_customer
+
+
+@pytest.fixture
+def create_user(api_client):
+    def do_create_user():
+        user = baker.make(User)
+        api_client.post('/auth/users/', model_to_dict(user))
+    return do_create_user
 
 
 @pytest.fixture
