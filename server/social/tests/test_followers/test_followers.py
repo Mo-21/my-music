11dc6443cf67fe24313f56447505bfc8266299dd
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-30 08:29:54 +0300

    test following actions

---
+++ server/social/tests/test_followers/test_followers.py
@@ -0,0 +1,51 @@
+from rest_framework import status
+import pytest
+
+
+@pytest.mark.django_db
+class TestFollower:
+    def test_follow_user_return_401_if_user_is_not_auth(self, api_client):
+        response = api_client.post('/social/followers/', data={})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_follow_user_return_400_if_user_is_auth_and_data_is_invalid(self, api_client, create_customer):
+        create_customer(is_authenticated=True)
+
+        response = api_client.post('/social/followers/', data={})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_follow_user_return_400_if_user_follows_himself(self, api_client, create_customer):
+        customer = create_customer(is_authenticated=True)
+
+        response = api_client.post(
+            '/social/followers/', data={'following_user': customer.id})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_follow_user_return_201_if_user_is_auth_and_data_is_valid(self, api_client, create_customer):
+        customer1 = create_customer(is_authenticated=True)
+        customer2 = create_customer(is_authenticated=True)
+
+        response = api_client.post(
+            '/social/followers/', data={'following_user': customer1.id})
+
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_unfollow_user_return_401_if_user_is_not_auth(self, api_client):
+        response = api_client.delete('/social/followers/1/')
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_unfollow_user_return_204_if_user_is_auth_and_data_is_valid(self, api_client, create_customer):
+        customer1 = create_customer(is_authenticated=True)
+        customer2 = create_customer(is_authenticated=True)
+        api_client.post(
+            '/social/followers/',
+            data={'following_user': customer1.id}
+        )
+
+        response = api_client.delete(f'/social/followers/{customer1.id}/')
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
