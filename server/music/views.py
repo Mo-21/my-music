442050b03680ce3cf7133e0398f73aae91b559d2
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-31 02:51:17 +0300

    Secure endpoints to auth users only

--- server/music/views.py
+++ server/music/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin
 from rest_framework.exceptions import NotAuthenticated
+from rest_framework.permissions import IsAuthenticated
+from .pagination import CustomPagination
 from .models import Genre, Artist, Song, Playlist, PlaylistItem
 from .serializers import (GenreSerializer, ArtistSerializer, SongSerializer, SongWithNewArtistSerializer,
                           PlaylistSerializer, PlaylistItemSerializer)
@@ -9,17 +11,20 @@ from .serializers import (GenreSerializer, ArtistSerializer, SongSerializer, Son
 class GenreMixin(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class ArtistMixin(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class SongViewSet(ModelViewSet):
     queryset = Song.objects.select_related(
         'genre').select_related('artist').all()
     serializer_class = SongSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_context(self):
         user = self.request.user
@@ -32,6 +37,7 @@ class SongViewSet(ModelViewSet):
 
 class SongWithNewArtistMixin(CreateModelMixin, GenericViewSet):
     serializer_class = SongWithNewArtistSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_context(self):
         user = self.request.user
@@ -45,6 +51,7 @@ class SongWithNewArtistMixin(CreateModelMixin, GenericViewSet):
 class PlaylistViewSet(ModelViewSet):
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_context(self):
         user = self.request.user
@@ -59,6 +66,7 @@ class PlaylistItemViewSet(ModelViewSet):
     queryset = PlaylistItem.objects.all()
     serializer_class = PlaylistItemSerializer
     http_method_names = ['get', 'post', 'delete']
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_context(self):
         return {'playlist_id': self.kwargs['playlist_pk']}
