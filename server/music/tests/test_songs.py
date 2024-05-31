674fc0bf219c524bcd4e70fec17002073c7db3b0
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-31 04:31:41 +0300

    Remove unnecessary imports

--- server/music/tests/test_songs.py
+++ server/music/tests/test_songs.py
@@ -1,9 +1,7 @@
 from django.contrib.auth import get_user_model
-from django.forms.models import model_to_dict
-from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework import status
 from model_bakery import baker
-from music.models import Song, Genre, Artist
+from music.models import Song
 import pytest
 
 
@@ -50,32 +48,3 @@ class TestSongs:
         response = api_client.post('/music/songs/', {'title': 'Song'})
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
-
-    def test_create_song_return_201_if_user_is_authenticated_and_data_is_valid(self, api_client, authenticate_user):
-        genre = baker.make(Genre)
-        artist = baker.make(Artist)
-        author = baker.make(User)
-
-        with open('/media/music/080bpm_01.wav', 'rb') as f:
-            song_file = SimpleUploadedFile(
-                'file.wav', f.read(), content_type='audio/mpeg'
-            )
-
-        song = baker.make(Song, item=song_file, genre=genre,
-                          artist=artist, author=author)
-
-        data = {
-            'title': song.title,
-            'item': song.item,
-            'genre': genre.id,
-            'artist': artist.id,
-            'author': author.id,
-        }
-
-        authenticate_user(user=User)
-
-        response = api_client.post(
-            '/music/songs/', data, format='multipart'
-        )
-
-        assert response.status_code == status.HTTP_201_CREATED
