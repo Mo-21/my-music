5678a744d2f6d0626938cfa6b7bf7cf7b287160e
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-01 04:19:38 +0300

    Filter likes and comment by object_id and content-type

--- server/interactions/views.py
+++ server/interactions/views.py
@@ -1,4 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import CommentFilter, LikeFilter
 from .models import Like, Comment
 from .serializers import LikeSerializer, CommentSerializer
 
@@ -6,6 +10,9 @@ from .serializers import LikeSerializer, CommentSerializer
 class LikeViewSet(ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = LikeFilter
 
     def get_serializer_context(self):
         return {'user_id': self.request.user.id}
@@ -14,6 +21,10 @@ class LikeViewSet(ModelViewSet):
 class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = CommentFilter
+    search_fields = ['text']
 
     def get_serializer_context(self):
         return {'user_id': self.request.user.id}
