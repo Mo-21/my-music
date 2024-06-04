75453ca9e6106d27dc31d20866a5d751f998266e
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-04 23:37:33 +0300

    Add tags endpoint

--- server/tags/views.py
+++ server/tags/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateModelMixin
+from .models import Tag
+from .serializers import TagSerializer
 
-# Create your views here.
+
+class TagViewSet(ListModelMixin, RetrieveModelMixin, CreateModelMixin, GenericViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
