579e5040794a00e8c276818a29725cd767f21c41
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-05 23:50:32 +0300

    Add registration image to Registration page

--- client/src/pages/Registration.tsx
+++ client/src/pages/Registration.tsx
@@ -1,5 +1,15 @@
+import registrationImage from "../assets/mymusic-homepage-photo.jpg";
+
 const Registration = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <img
+        className="w-full h-screen object-cover"
+        src={registrationImage}
+        alt="registrationImage"
+      />
+    </div>
+  );
 };
 
 export default Registration;
