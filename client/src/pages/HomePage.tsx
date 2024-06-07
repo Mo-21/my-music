1c69bb13a42c3490c6b1d289239187f6071674d6
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-07 06:16:00 +0300

    Update HomePage to fetch and display posts using react-query

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,10 +1,27 @@
 import { Container } from "@radix-ui/themes";
+import { usePosts } from "../hooks/usePosts";
+import React from "react";
 
 const HomePage = () => {
+  const { data, error, isLoading } = usePosts();
+
+  if (error) return <Container>Error: {error.message}</Container>;
+  if (isLoading) return <Container>Loading...</Container>;
+
   return (
     <Container>
       <h1>Home Page</h1>
       <p>Welcome to the home page</p>
+      {data?.pages.map((group, i) => (
+        <React.Fragment key={i}>
+          {group.results.map((post) => (
+            <div key={post.id}>
+              <h2>{post.createdAt}</h2>
+              <p>{post.content}</p>
+            </div>
+          ))}
+        </React.Fragment>
+      ))}
     </Container>
   );
 };
