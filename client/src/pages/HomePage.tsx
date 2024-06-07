3bfa632bc660529dafb8b4b843d2a200876a2ff9
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-07 05:22:41 +0300

    Add HomePage component

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@radix-ui/themes";
+
+const HomePage = () => {
+  return (
+    <Container>
+      <h1>Home Page</h1>
+      <p>Welcome to the home page</p>
+    </Container>
+  );
+};
+
+export default HomePage;
