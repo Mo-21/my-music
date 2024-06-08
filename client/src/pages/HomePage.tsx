a32767aa93fa27fe6be8e0b7effe81f5bd827829
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-08 03:47:30 +0300

    Update HomePage to display infinite posts with react-query

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,27 +1,34 @@
-import { Container } from "@radix-ui/themes";
+import { Box, Button, Container } from "@radix-ui/themes";
 import { usePosts } from "../hooks/usePosts";
 import React from "react";
 
 const HomePage = () => {
-  const { data, error, isLoading } = usePosts();
+  const { data, error, isLoading, hasNextPage, fetchNextPage } = usePosts();
 
   if (error) return <Container>Error: {error.message}</Container>;
   if (isLoading) return <Container>Loading...</Container>;
 
+  console.log(data);
+
   return (
     <Container>
-      <h1>Home Page</h1>
-      <p>Welcome to the home page</p>
-      {data?.pages.map((group, i) => (
-        <React.Fragment key={i}>
-          {group.results.map((post) => (
-            <div key={post.id}>
-              <h2>{post.createdAt}</h2>
-              <p>{post.content}</p>
-            </div>
-          ))}
-        </React.Fragment>
-      ))}
+      <Box className="prose">
+        <h1>Home Page</h1>
+        <p>Welcome to the home page</p>
+        {data?.pages.map((group, i) => (
+          <React.Fragment key={i}>
+            {group.results.map((post) => (
+              <div className="border-2 border-red-500" key={post.id}>
+                <h2>{post.id}</h2>
+                <p>{post.content}</p>
+              </div>
+            ))}
+          </React.Fragment>
+        ))}
+      </Box>
+      {hasNextPage ? (
+        <Button onClick={() => fetchNextPage()}>Load More</Button>
+      ) : null}
     </Container>
   );
 };
