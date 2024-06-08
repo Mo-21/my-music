197103885a85b5d930e1fb9d55ca9990bc20fe04
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-09 00:38:37 +0300

    Add MusicTab and update Feed component

--- client/src/components/Feed.tsx
+++ client/src/components/Feed.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@radix-ui/themes";
 import PostsTab from "./PostsTab";
+import MusicTab from "./MusicTab";
 
 const Feed = () => {
   return (
     <Box>
       <PostsTab />
+      <MusicTab />
     </Box>
   );
 };
