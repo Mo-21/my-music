8a9fc795e4818857704ec7565e3c379672d2739b
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-14 01:08:04 +0300

    Add react-h5-audio-player package for audio playback in SongList component

--- client/src/components/SongList.tsx
+++ client/src/components/SongList.tsx
@@ -1,20 +1,25 @@
 import { Flex, Text, Card, Heading } from "@radix-ui/themes";
 import { Song } from "../types/musicTypes";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 const SongList = ({ songs }: { songs: Song[] }) => {
   return (
     <>
       {songs.map((song) => (
-        <Card role="feed" className="h-40" key={song.id}>
+        <Card role="feed" className="h-40 bg-green-100" key={song.id}>
           <Flex direction="column" gap="3">
             <Flex justify="between">
-              <Flex gap="2">
+              <Flex direction="column">
                 <Heading size="3">{song.title}</Heading>
-                <Heading size="3">{song.artist.name}</Heading>
+                <Heading size="2">{song.artist.name}</Heading>
               </Flex>
               <Text>{song.created_at.split("T")[0]}</Text>
             </Flex>
-            <Text>{song.duration}</Text>
+            <AudioPlayer
+              autoPlay={false}
+              src={"http://localhost:8000" + song.item}
+            />
           </Flex>
         </Card>
       ))}
