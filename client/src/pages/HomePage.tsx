762aa68230c2c05e559834742a0fc9aec8f40d3a
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-16 01:36:46 +0300

    Add CreatePost component and update HomePage layout

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,12 +1,16 @@
 import { Flex } from "@radix-ui/themes";
 import MusicTab from "../components/MusicTab";
 import PostsTab from "../components/PostsTab";
+import CreatePost from "../components/CreatePost";
 
 const HomePage = () => {
   return (
     <Flex justify="center" p="2">
       <MusicTab />
-      <PostsTab />
+      <Flex mt="2" px="2" direction="column">
+        <CreatePost />
+        <PostsTab />
+      </Flex>
       <MusicTab />
     </Flex>
   );
