eba550d70010b63c31b3435814c486cf2a2e3eee
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-15 21:12:42 +0300

    Refactor PostsList component to improve code organization and readability

--- client/src/components/PostsList.tsx
+++ client/src/components/PostsList.tsx
@@ -1,11 +1,9 @@
 import { Flex, Text, Card, Heading, Separator } from "@radix-ui/themes";
-import { Post } from "../types/socialTypes";
+import { Post, Comment, PostAuthor } from "../types/socialTypes";
 import LikeButton from "./LikeButton";
 import CommentForm from "./CommentForm";
 import CustomerAvatar from "./CustomerAvatar";
 import MembershipBadge from "./MembershipBadge";
-
-// TODO: styles refactor for: 1) the comments section 2) the like button animation
 
 const PostsList = ({ posts }: { posts: Post[] }) => {
   return (
@@ -14,47 +12,62 @@ const PostsList = ({ posts }: { posts: Post[] }) => {
         <Card role="feed" className="min-h-[10rem] mb-4" key={post.id}>
           <Flex direction="column" gap="3" p="2">
             <Flex justify="between">
-              <Flex gap="2">
-                <CustomerAvatar
-                  src={post.author.profile_image}
-                  fallback={post.author.user.first_name[0]}
-                />
-                <Heading size="3">
-                  {post.author.user.first_name} {post.author.user.last_name}
-                </Heading>
-              </Flex>
+              <CustomerDetails author={post.author} />
               <Text>{post.created_at.split("T")[0]}</Text>
             </Flex>
             <Text>{post.content}</Text>
-            <Flex justify="between">
-              <Flex align="center" gap="4">
-                <Text className="text-lg">{post.likes_count}</Text>
-                <LikeButton postId={post.id} />
-              </Flex>
-              <Flex align="center" gap="4">
-                <Text className="text-lg">{post.comments_count}</Text>
-              </Flex>
-            </Flex>
+            <PostStats post={post} />
             <Separator size="4" />
             <CommentForm postId={post.id} />
-            {post.comments_count > 0 && (
+            {post.comments_count > 0 && post.comments && (
               <Flex direction="column">
-                {post.comments?.map((c) => (
-                  <Flex mt="3" key={c.id} gap="2">
-                    <CustomerAvatar
-                      src={c.user.profile_image}
-                      fallback={c.user.user.first_name[0]}
-                    />
-                    <Text>{c.text}</Text>
-                    <MembershipBadge
-                      membershipStatus={c.user.membership_status}
-                    />
-                  </Flex>
-                ))}
+                <Comments comments={post.comments} />
               </Flex>
             )}
           </Flex>
         </Card>
+      ))}
+    </>
+  );
+};
+
+const CustomerDetails = ({ author }: { author: PostAuthor }) => {
+  return (
+    <Flex align="center" gap="2">
+      <CustomerAvatar
+        src={author.profile_image}
+        fallback={author.user.first_name[0]}
+      />
+      <Heading size="3">
+        {author.user.first_name} {author.user.last_name}
+      </Heading>
+    </Flex>
+  );
+};
+
+const PostStats = ({ post }: { post: Post }) => {
+  return (
+    <Flex justify="between">
+      <Flex align="center" gap="4">
+        <Text className="text-lg">{post.likes_count}</Text>
+        <LikeButton postId={post.id} />
+      </Flex>
+      <Flex align="center" gap="4">
+        <Text className="text-lg">{post.comments_count}</Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+const Comments = ({ comments }: { comments: Comment[] }) => {
+  return (
+    <>
+      {comments.map((c) => (
+        <Flex mt="3" align="center" key={c.id} gap="2">
+          <CustomerDetails author={c.user} />
+          <MembershipBadge membershipStatus={c.user.membership_status} />
+          <Text>{c.text}</Text>
+        </Flex>
       ))}
     </>
   );
