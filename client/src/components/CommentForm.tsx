bab4d0f6b37cf1542766210c121f6c35e242a01d
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-15 21:12:48 +0300

    Add @radix-ui/react-icons package and update dependencies

--- client/src/components/CommentForm.tsx
+++ client/src/components/CommentForm.tsx
@@ -1,6 +1,7 @@
 import { Button, TextArea } from "@radix-ui/themes";
 import { useRef } from "react";
 import { useCreateComment } from "../hooks/useCreateComment";
+import { PaperPlaneIcon } from "@radix-ui/react-icons";
 
 const CommentForm = ({ postId }: { postId: number }) => {
   const ref = useRef<HTMLTextAreaElement>(null);
@@ -12,14 +13,17 @@ const CommentForm = ({ postId }: { postId: number }) => {
         e.stopPropagation();
         e.preventDefault();
 
-        if (!ref.current) return;
+        if (!ref.current || ref.current.value === "") return;
 
         mutate({ text: ref.current.value, postId });
         ref.current.value = "";
       }}
+      className="grid grid-cols-12 gap-2"
     >
-      <TextArea ref={ref} placeholder="Comment" />
-      <Button>Create</Button>
+      <TextArea ref={ref} placeholder="Comment" className="col-span-11" />
+      <Button className="h-full col-span-1">
+        <PaperPlaneIcon />
+      </Button>
     </form>
   );
 };
