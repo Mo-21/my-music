a9148fcf9ca5b866b2ef30f885e00bbc1490fb07
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-16 01:36:32 +0300

    Add CreatePost component for creating new posts

---
+++ client/src/components/CreatePost.tsx
@@ -0,0 +1,33 @@
+import { Button, TextArea } from "@radix-ui/themes";
+import { useRef } from "react";
+import { useCreatePost } from "../hooks/useCreatePost";
+
+const CreatePost = () => {
+  const ref = useRef<HTMLTextAreaElement>(null);
+  const { mutateAsync } = useCreatePost();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (!ref.current || ref.current.value === "") return;
+
+    await mutateAsync({ content: ref.current.value });
+
+    ref.current.value = "";
+  };
+
+  return (
+    <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col gap-1">
+      <TextArea
+        ref={ref}
+        size="3"
+        placeholder="What's on your mind?"
+        className="rounded-md"
+      />
+      <Button>Post</Button>
+    </form>
+  );
+};
+
+export default CreatePost;
