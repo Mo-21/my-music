1c5a017a9090bffd0335d031fbef70f2b65134dd
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-06-16 20:02:21 +0300

    Update useGetCurrentCustomer hook and add useCustomerProfile hook

--- client/src/hooks/useGetCurrentCustomer.ts
+++ client/src/hooks/useGetCurrentCustomer.ts
@@ -1,13 +1,30 @@
 import { useQuery } from "@tanstack/react-query";
 import { Customer } from "../types/socialTypes";
 import { axiosInstance } from "../services/axiosInstance ";
+import { useCurrentCustomer } from "../services/store";
 
 export const useGetCurrentCustomer = () => {
+  const setCustomer = useCurrentCustomer((c) => c.setCustomer);
+
   return useQuery<Customer, Error>({
     queryKey: ["current_customer"],
     queryFn: async () => {
       const response = await axiosInstance
         .get<Customer>("/social/customers/me/")
+        .then((res) => res.data);
+
+      setCustomer(response);
+      return response;
+    },
+  });
+};
+
+export const useCustomerProfile = ({ customerId }: { customerId: number }) => {
+  return useQuery<Customer, Error>({
+    queryKey: ["current_customer"],
+    queryFn: async () => {
+      const response = await axiosInstance
+        .get<Customer>(`/social/customers/${customerId}/`)
         .then((res) => res.data);
 
       return response;
